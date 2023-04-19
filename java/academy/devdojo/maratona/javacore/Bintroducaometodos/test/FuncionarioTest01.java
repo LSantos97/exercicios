019e58f1fd44498a9d9936c96f1ea12717bf8102
Lohan Santos <lohan.santos@live.com>
2023-04-19 17:17:46 -0300

    DevDojo

--- java/academy/devdojo/maratona/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ java/academy/devdojo/maratona/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,10 +5,11 @@ import academy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Sanji";
-        funcionario.idade = 26;
-        funcionario.salarios = new double[]{1200,987.32,2000};
+        funcionario.setNome("Sanji");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1200,987.32,2000});
 
         funcionario.imprime();
+        System.out.println("Média "+funcionario.getMedia());
     }
 }
