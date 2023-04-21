aba3c324056c331cd6f8b5e1536532363065586f
Lohan Santos <lohan.santos@live.com>
2023-04-21 18:19:32 -0300

    att construtores

--- java/academy/devdojo/maratona/javacore/Dconstrutores/test/AnimeTest01.java
+++ java/academy/devdojo/maratona/javacore/Dconstrutores/test/AnimeTest01.java
@@ -6,9 +6,7 @@ import academy.devdojo.maratona.javacore.Dconstrutores.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
 
-        Anime anime = new Anime("HxH", "TV", 12, "Drama");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("HxH", "TV", 12, "Drama", "A-1 Pictures");
         anime.imprime();
-        anime2.imprime();
     }
 }
